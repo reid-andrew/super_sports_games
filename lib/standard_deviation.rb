1ff50ba2c9ede6a019902bb9c03c94140453671e
Andrew Reid <areid.sj@gmail.com>
2020-01-30 13:39:45 -0700

    Write standard deviation class

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,36 @@
-ages = [24, 30, 18, 20, 41]
+# ages = [24, 30, 18, 20, 41]
 
-# Your code here for calculating the standard deviation
+class Standard_Deviation
+  def initialize(input)
+    @input = input
+    @sum = 0.0
+    @count = 0.0
+    @mean = 0.0
+    @input_minus_mean = []
+    @sum_squared = 0.0
+    @sum_squared_divided = 0.0
+    @std_dev = 0.0
+  end
 
-# When you find the standard deviation, print it out
+  def standard_deviation
+    @input.each do |num|
+      @sum += num
+      @count += 1.0
+    end
+
+    @mean = @sum / @count
+
+    @input.each do |num|
+      @input_minus_mean << num - @mean
+    end
+
+    @input_minus_mean.each do |num|
+      num = num * num
+      @sum_squared += num
+    end
+
+    @sum_squared_divided = @sum_squared / @count
+
+    @std_dev = Math.sqrt(@sum_squared_divided).round(2)
+  end
+end
---
+++ runner.rb
@@ -0,0 +1,3 @@
+require './lib/standard_deviation'
+sd = Standard_Deviation.new([24, 30, 18, 20, 41])
+puts sd.standard_deviation
